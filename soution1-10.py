fbf9b155ff362c7dc9f0ef81fed631d78d9a3856
suhyeon lee <81307924+suhyeon0111@users.noreply.github.com>
2021-07-20 23:55:54 +0900

    코드업 기초 100제 Python 1번~10번

---
+++ soution1-10.py
@@ -0,0 +1,14 @@
+print("Hello")
+print("Hello World")
+print("Hello\nWorld")
+print("\'Hello\'")
+print("\"Hello World\"")
+print("\"!@#$%^&*()\"")
+print("C:\Download\hello.cpp")
+print('\u250C\u252C\u2510')
+print('\u251C\u253C\u2524')
+print('\u2514\u2534\u2518')
+
+a = int(input())
+print(a)
+
