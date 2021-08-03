6f2766e2a9dde2aa3e6a8d1ca4564c09ebd178a6
suhyeon lee <81307924+suhyeon0111@users.noreply.github.com>
2021-08-03 22:11:41 +0900

    노마드 코더 [파이썬으로 웹 스크립트 만들기] 강좌-리스트 실습

---
+++ list_in.py
@@ -0,0 +1,10 @@
+days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+
+print("Mon" in days)
+print("Man" in days)
+print(days[3])
+print(len(days))
+days.append("Sat")
+print(days)
+days.reverse()
+print(days)
